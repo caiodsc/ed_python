c92e7f44ff51cbdc87cd0eddb4c3aab772d75637
Caio <caio.dscamara@gmail.com>
2017-11-09 02:37:01 -0200

    Lista ligada, parte 2.

--- Lista Ligada/linked_list.py
+++ Lista Ligada/linked_list.py
@@ -54,7 +54,55 @@ class LinkedList:
                         prev_node = curr_node
                         curr_node = curr_node.getNext()
                         curr_index += 1
-                    # atualiza o tamanho da lista
-                    self.len_list += 1
+            # atualiza o tamanho da lista
+            self.len_list += 1
+
+    def pop(self, index):
+        if not self.empty() and index >=0 and index < self.len_list:
+            flag_remove = False
+            if self.first.getNext() == None:
+                # possui apenas 1 elemento
+                self.first = None
+                self.last = None
+                flag_remove = True
+            elif index == 0:
+                #remove do início, mas possui mais de 1 elemento
+                self.first = self.first.getNext()
+                flag_remove = True
+            else:
+                '''Se chegou até aqui é porque a lista possui mais
+                de um elemento e a remoção não é no início'''
+                prev_node = self.first
+                curr_node = self.first.getNext()
+                curr_index = 1
+
+                while curr_node != None:
+                    if index == curr_index:
+                        # o próximo do anterior aponta para o próximo do nó corrente
+                        prev_node.setNext(curr_node.getNext())
+                        curr_node.setNext(None)
+                        flag_remove = True
+                        break
+                    prev_node = curr_node
+                    curr_node = curr_node.getNext()
+                    curr_index += 1
+            if flag_remove:
+                # atualiza o tamanho da lista
+                self.len_list -= 1
+
+    def empty(self):
+        if self.first == None:
+            return True
+        return False
+
+    def length(self):
+        return self.len_list
+
+    def show(self):
+        curr_node = self.first
+        while curr_node != None:
+            print(curr_node.getLabel(), end = ' ')
+            curr_node = curr_node.getNext()
+        print('')
 
 
